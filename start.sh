833b4df0dfa4b3effad1f65af2fe03f0e8a015b9
Gerardo Venegas <gmatiasvc@hotmail.com>
2025-10-08 10:06:52 -0500

    added a script utility to start the tailwind cli

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "TAILWIND CLI STARTED"
+
+if [ -n "$BASH_SOURCE" ]; then
+    SCRIPT_PATH="${BASH_SOURCE[0]}"
+else
+    SCRIPT_PATH="$0"
+fi
+
+SCRIPT_DIR=$(cd -P "$(dirname "$SCRIPT_PATH")" && pwd)
+
+INPUT_DIR="$SCRIPT_DIR/css/input.css"
+TAILWIND_DIR="$SCRIPT_DIR/css/tailwind.css"
+
+npx @tailwindcss/cli -i $INPUT_DIR -o $TAILWIND_DIR --watch
