143a25b9260a2aeb80139e1f5f46e167f55c17c5
Gerardo Venegas <gmatiasvc@hotmail.com>
2025-10-15 21:39:47 -0500

    refactor: update Tailwind CSS classes for improved responsiveness and layout

--- start.sh
+++ start.sh
@@ -13,4 +13,5 @@ SCRIPT_DIR=$(cd -P "$(dirname "$SCRIPT_PATH")" && pwd)
 INPUT_DIR="$SCRIPT_DIR/css/input.css"
 TAILWIND_DIR="$SCRIPT_DIR/css/tailwind.css"
 
+code $SCRIPT_DIR
 npx @tailwindcss/cli -i $INPUT_DIR -o $TAILWIND_DIR --watch
