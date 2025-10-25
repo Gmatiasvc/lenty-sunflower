49d17e337b80cb1e1eb575725d079b402b62592d
Gerardo Venegas <gmatiasvc@hotmail.com>
2025-10-25 00:33:10 -0500

    refactor: update CSS variable names and streamline style observer for improved clarity and maintainability

--- js/style_observer.js
+++ js/style_observer.js
@@ -18,26 +18,28 @@ document.addEventListener("DOMContentLoaded", () => {
 		  // 2. Read the "source" variables from that section's CSS
 		  const bg1 = computedStyle.getPropertyValue('--dark-black-1').trim();
 		  const bg2 = computedStyle.getPropertyValue('--dark-black-2').trim();
-		  const wh = computedStyle.getPropertyValue('--dark-white').trim();
-		  const re = computedStyle.getPropertyValue('--dark-red').trim();
-		  const or = computedStyle.getPropertyValue('--dark-orange').trim();
-		  const ye = computedStyle.getPropertyValue('--dark-yellow').trim();
-		  const bl = computedStyle.getPropertyValue('--dark-blue').trim();
-		  const gr = computedStyle.getPropertyValue('--dark-green').trim();
-		  const pu = computedStyle.getPropertyValue('--dark-purple').trim();
-  
+		  const whi = computedStyle.getPropertyValue('--dark-white').trim();
+		  const co1 = computedStyle.getPropertyValue('--dark-text-1').trim();
+		  const co2 = computedStyle.getPropertyValue('--dark-text-2').trim();
+		  const co3 = computedStyle.getPropertyValue('--dark-text-3').trim();
+		  const co4 = computedStyle.getPropertyValue('--dark-text-4').trim();
+		  const co5 = computedStyle.getPropertyValue('--dark-text-5').trim();
+		  const co6 = computedStyle.getPropertyValue('--dark-text-6').trim();
+		  const hyp = computedStyle.getPropertyValue('--dark-hyperlink').trim();
 		  // 3. Update the "active" global variables on the :root
 		  root.style.setProperty('--color-bg-1', bg1);
 		  root.style.setProperty('--color-bg-2', bg2);
 
 		  
-		  root.style.setProperty('--color-white',wh);
-		  root.style.setProperty('--color-red',re);
-		  root.style.setProperty('--color-orange',or);
-		  root.style.setProperty('--color-yellow',ye);
-		  root.style.setProperty('--color-green',gr);
-		  root.style.setProperty('--color-blue',bl);
-		  root.style.setProperty('--color-purple',pu);
+		  root.style.setProperty('--color-white', whi);
+		  root.style.setProperty('--color-text-1',co1);
+		  root.style.setProperty('--color-text-2',co2);
+		  root.style.setProperty('--color-text-3',co3);
+		  root.style.setProperty('--color-text-4',co4);
+		  root.style.setProperty('--color-text-5',co5);
+		  root.style.setProperty('--color-text-6',co6);
+
+		  root.style.setProperty('--color-hyperlink',hyp);
 		}
 	  });
 	};
